f98d0b76ffa254970c0c32e538cbd7d3e2f493f2
Ivanova Margo <yuqiivi154@gmail.com>
2026-06-12 16:03:36 +0300

    Применен gitignore

--- obj/Release/net9.0/TournamentApp.AssemblyInfo.cs
+++ obj/Release/net9.0/TournamentApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TournamentApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbd12be3b1889ef42c316e5bcc2aabb62e81dbf3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b88e466e6d05aa907634bc911223975830250aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("TournamentApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TournamentApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
